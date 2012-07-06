e866d41b1d4c01d94c8abe542c93c950770577f4
Nikita <abracham.mitchell@gmail.com>
2012-07-06 13:59:49 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#Netty-socketio Demo
+
+Demo for Netty-socketio project.
+
+#Usage example
+
+1. Build or install Netty-socketio lib to your maven repository.
+2. Switch to /server folder and build server by maven.
+3. Run server by follow command: mvn exec:java
+4. Run client in browser, by opening* file /client/index.html
+
+* If you want to open index.html in Chrome browser you need to host it somewhere (nginx, for example),
+ because of absence of "origin" http header in this case.
+   
---
+++ server/src/main/java/com/corundumstudio/socketio/demo/Luncher.java
@@ -0,0 +1,30 @@
+package com.corundumstudio.socketio.demo;
+
+import com.corundumstudio.socketio.listener.*;
+import com.corundumstudio.socketio.*;
+
+public class Luncher {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Configuration config = new Configuration();
+        config.setHostname("localhost");
+        config.setPort(9092);
+
+        final SocketIOServer server = new SocketIOServer(config);
+        server.addJsonObjectListener(new DataListener<Object>() {
+            @Override
+            public void onData(SocketIOClient client, Object data) {
+                // broadcast messages to all clients
+                server.getBroadcastOperations().sendJsonObject(data);
+            }
+        });
+
+        server.start();
+
+        Thread.sleep(Integer.MAX_VALUE);
+
+        server.stop();
+    }
+
+}
