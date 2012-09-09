bba55bf1d6bc15bad9010a267c36d6c1a118ac54
Nikita <abracham.mitchell@gmail.com>
2012-09-09 13:38:59 +0400

    Namespaces example added

---
+++ server/src/main/java/com/corundumstudio/socketio/demo/NamespaceChatLauncher.java
@@ -0,0 +1,40 @@
+package com.corundumstudio.socketio.demo;
+
+import com.corundumstudio.socketio.listener.*;
+import com.corundumstudio.socketio.*;
+
+public class NamespaceChatLauncher {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Configuration config = new Configuration();
+        config.setHostname("localhost");
+        config.setPort(9092);
+
+        final SocketIOServer server = new SocketIOServer(config);
+        final SocketIONamespace chat1namespace = server.addNamespace("/chat1");
+        chat1namespace.addJsonObjectListener(ChatObject.class, new DataListener<ChatObject>() {
+            @Override
+            public void onData(SocketIOClient client, ChatObject data) {
+                // broadcast messages to all clients
+                chat1namespace.getBroadcastOperations().sendJsonObject(data);
+            }
+        });
+
+        final SocketIONamespace chat2namespace = server.addNamespace("/chat2");
+        chat2namespace.addJsonObjectListener(ChatObject.class, new DataListener<ChatObject>() {
+            @Override
+            public void onData(SocketIOClient client, ChatObject data) {
+                // broadcast messages to all clients
+                chat2namespace.getBroadcastOperations().sendJsonObject(data);
+            }
+        });
+
+        server.start();
+
+        Thread.sleep(Integer.MAX_VALUE);
+
+        server.stop();
+    }
+
+}
