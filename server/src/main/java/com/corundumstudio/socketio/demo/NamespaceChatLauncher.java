c8ba2c823c71feeb91c758916c8b73a483c2196a
Nikita <abracham.mitchell@gmail.com>
2014-09-24 11:46:47 +0400

    NamespaceChatLauncher fixed. #13

--- server/src/main/java/com/corundumstudio/socketio/demo/NamespaceChatLauncher.java
+++ server/src/main/java/com/corundumstudio/socketio/demo/NamespaceChatLauncher.java
@@ -13,20 +13,20 @@ public class NamespaceChatLauncher {
 
         final SocketIOServer server = new SocketIOServer(config);
         final SocketIONamespace chat1namespace = server.addNamespace("/chat1");
-        chat1namespace.addJsonObjectListener(ChatObject.class, new DataListener<ChatObject>() {
+        chat1namespace.addEventListener("message", ChatObject.class, new DataListener<ChatObject>() {
             @Override
             public void onData(SocketIOClient client, ChatObject data, AckRequest ackRequest) {
                 // broadcast messages to all clients
-                chat1namespace.getBroadcastOperations().sendJsonObject(data);
+                chat1namespace.getBroadcastOperations().sendEvent("message", data);
             }
         });
 
         final SocketIONamespace chat2namespace = server.addNamespace("/chat2");
-        chat2namespace.addJsonObjectListener(ChatObject.class, new DataListener<ChatObject>() {
+        chat2namespace.addEventListener("message", ChatObject.class, new DataListener<ChatObject>() {
             @Override
             public void onData(SocketIOClient client, ChatObject data, AckRequest ackRequest) {
                 // broadcast messages to all clients
-                chat2namespace.getBroadcastOperations().sendJsonObject(data);
+                chat2namespace.getBroadcastOperations().sendEvent("message", data);
             }
         });
 
